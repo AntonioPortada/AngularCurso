c659faa71ee179d473177ed1e6d22dffc7d0d37c
Antonio Portada <antonio_portada25@hotmail.com>
2020-05-11 14:50:19 -0500

    single page aplication

---
+++ 02-spa/src/app/components/filtro/filtro.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-filtro',
+  templateUrl: './filtro.component.html'
+})
+export class FiltroComponent implements OnInit {
+
+  heroes: any[] = [];
+  termino: string;
+
+  constructor( private activatedRoute: ActivatedRoute,
+               private _heroesService: HeroesService ) { }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe( params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes( params['termino'] );
+    });
+  }
+}
